83d6390a61f242692bca5f4925ec5aa80cb3b73e
Belood <alexandre.ung96@gmail.com>
2018-04-26 10:59:42 -0400

    Ajoutez des fichiers projet.

---
+++ TeethCabinet/Models/BlockUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace System.Web.Mvc
+{
+    public static class BlockUser
+    {
+        public static string GetUserRole(this HtmlHelper html)
+        {
+            string CurrentUserRole = "Admin";
+            return CurrentUserRole;
+        }
+    }
+}
